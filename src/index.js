ff5ea25bfa29d14fb088e5855d2900ff0abb04c9
Ricahrd Walkden <rswalkden@gmail.com>
2019-08-08 12:03:42 +0100

    Added functionality to roll mutpliple dice of different sizes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
+const performRolls = (rand, dieCount, dieSize) => {
+  return Array(Number(dieCount)).fill(0).map(_ => Math.floor(rand() * Number(dieSize) + 1))
+}
+
 const diceRoller = (command, rand = Math.random) => {
-  return 4
+  const reduced = command.replace(/\d+[d]\d+(k\d+)?/g, (match) => {
+    const [dieCount, dieSize, keep] = match.split(/[dk]/g)
+    console.log(dieCount, dieSize, keep)
+    const rolls = performRolls(rand, dieCount, dieSize)
+    console.log(rolls)
+    return rolls.reduce((a, b) => a + b)
+  })
+  console.log(reduced)
+
+  // let [die, value] = command.split('+')
+  // die = die.trim()
+  // value = value ? value.trim() : 0
+  // const [count, dieSize] = die.split(/\d+d/)
+
+  // return count * Math.floor(rand() * dieSize + 1) + +value
+  return +reduced.split('+').reduce((a, b) => Number(a) + Number(b))
 }
 
 module.exports = diceRoller
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,30 @@
 const diceRoller = require('../src')
+const sinon = require('sinon')
 
 describe('The dice roller', () => {
   describe('Simple Rolls', () => {
     it('should complete simple rolls', () => {
       const fakeRandom = () => 0.5
       expect(diceRoller('1d6', fakeRandom)).toEqual(4)
+
+      expect(diceRoller('2d6', fakeRandom)).toEqual(8)
+
+      expect(diceRoller('2d4', fakeRandom)).toEqual(6)
+
+      expect(diceRoller('3d12', fakeRandom)).toEqual(21)
+    })
+
+    it('should work with addtion', () => {
+      const fakeRandom = () => 0.5
+      expect(diceRoller('1d6 + 2', fakeRandom)).toEqual(4 + 2)
+    })
+
+    it('should keep or drop the highest value die/s rolled', () => {
+      const fakeRandom = () => sinon.stub()
+        .onFirstCall(0.5)
+        .onSecondCall(0.75)
+        .onThirdCall(0.25)
+      expect(diceRoller('3d20k2', fakeRandom)).toEqual(27)
     })
   })
 })
