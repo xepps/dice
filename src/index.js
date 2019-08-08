56f5ffc052943d168177efdf1b4919d2dcb8ea0b
Kristian Epps <kristian@xepps.com>
2019-08-08 12:02:27 +0100

    going for it

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
+const rollDice = (command, rand) => {
+  if (!command.includes('d')) return +command
+
+  const dice = command.split('d')
+  let acc = 0
+  for (let i = 0; i < dice[0]; i += 1) {
+    acc += Math.floor((dice[1] * rand()) + 1)
+  }
+  return acc
+}
+
 const diceRoller = (command, rand = Math.random) => {
-  return 4
+  return command.split('+')
+    .map(s => s.trim())
+    .map(e => rollDice(e, rand))
+    .reduce((acc, curr) => acc + curr, 0)
 }
 
 module.exports = diceRoller
--- test/index.spec.js
+++ test/index.spec.js
@@ -6,5 +6,40 @@ describe('The dice roller', () => {
       const fakeRandom = () => 0.5
       expect(diceRoller('1d6', fakeRandom)).toEqual(4)
     })
+    it('should complete more simple rolls', () => {
+      const fakeRandom = () => 0.25
+      expect(diceRoller('1d6', fakeRandom)).toEqual(2)
+    })
+    it('should complete even more simple rolls', () => {
+      const fakeRandom = () => 0.75
+      expect(diceRoller('1d6', fakeRandom)).toEqual(5)
+    })
+  })
+
+  describe('Rolling more than one dice', () => {
+    it('should roll 2 dice', () => {
+      const fakeRandom = () => 0.5
+      expect(diceRoller('2d6', fakeRandom)).toEqual(8)
+    })
+
+    it('should roll 2 different dice', () => {
+      let randCalls = 0
+      const fakeRandom = () => {
+        if (randCalls === 0) {
+          randCalls += 1
+          return 0.5
+        }
+        return 0.25
+      }
+
+      expect(diceRoller('2d6', fakeRandom)).toEqual(6)
+    })
+  })
+
+  describe('simple mathematices', () => {
+    it('should add on a simple number', () => {
+      const fakeRandom = () => 0.5
+      expect(diceRoller('1d20+5', fakeRandom)).toEqual(16)
+    })
   })
 })
